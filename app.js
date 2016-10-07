7ddcc86d89ef483e19f5118aaa8218b4abde20ac
Manni <manuelkenna@gmail.com>
2016-10-07 15:04:57 -0500

    initial submit

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var expressControllers = require('express-controller');
 var app = express();
+var router = express.Router();
+
+// controllers setup
+app.use(router);
+expressControllers.setDirectory(path.join(__dirname + '/controllers')).bind(router);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,9 +24,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ controllers/HomeController.js
@@ -0,0 +1,6 @@
+module.exports = {
+    get_index: function (req, res) {
+        console.log('Test');
+        res.send("Home index page test");
+    }
+}
